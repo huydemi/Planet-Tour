42c874a56bad01f15124a5ecd42f1239cb8413b3
Dang Quoc Huy <huysamdua@yahoo.com>
2018-09-10 14:27:03 +0700

    Installing the Remote Config Library

--- Planet Tour/AppDelegate.swift
+++ Planet Tour/AppDelegate.swift
@@ -28,6 +28,7 @@
 
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,6 +36,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    FirebaseApp.configure()
+    
     return true
   }
 }
